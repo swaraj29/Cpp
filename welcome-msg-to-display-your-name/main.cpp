2918541fadbc95e8505809f06f03b10c50e0cac4
swaraj kumar <028swarajkumar@gmail.com>
2023-02-26 03:28:33 +0530

    msg

---
+++ welcome-msg-to-display-your-name/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"may i know your name"<<endl;
+    getline(cin,name);
+    cout<<"welcome mr/mrs"<<name;
+    return 0;
+}
