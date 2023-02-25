308340e296b005560f533602df5162e50f0f2ea3
swaraj kumar <028swarajkumar@gmail.com>
2023-02-26 03:53:48 +0530

    area

---
+++ finding-area-of-triangle/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float b,h,a;
+    cout<<"enter base & height";
+    cin>>b>>h;
+    a=(b*h)/2;
+    cout<<"area of triangle is"<<a;
+    return 0;
+    
+}
