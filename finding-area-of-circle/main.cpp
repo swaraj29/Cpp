8344fc5bb3a1a3772c20a9338d271cfe19a966dd
swaraj kumar <028swarajkumar@gmail.com>
2023-02-26 04:13:17 +0530

    area

---
+++ finding-area-of-circle/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float radius;
+    float area;
+    
+    cout<<"enter radius of circle"<<endl;
+    cin>>radius;
+    
+    area=3.1425*radius*radius;
+    cout<<"area of circle is"<<area;
+    
+    return 0;
+}
